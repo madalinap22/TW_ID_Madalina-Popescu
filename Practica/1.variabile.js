e280babb29ab85de2722393ebf391ddad63f0b2e
madalinap22 <popescumadalina.gl@gmail.com>
2024-11-09 13:46:33 +0200

    variabile

---
+++ Practica/1.variabile.js
@@ -0,0 +1,19 @@
+var x = 5;
+const y = 7;
+let z = 9;
+
+console.log(x);
+
+var a;
+console.log(a);
+
+
+if(true)
+{
+    var b = 9;
+    let c = 10;
+console.log("variabila let",c);
+}
+
+console.log(b);
+//console.log(c); //eroare  "c is not defined"
